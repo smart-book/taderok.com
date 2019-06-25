2c7aa14474960d0c0f673f3d73175273913b681d
unknown <skander.ellouze@esprit.tn>
2019-06-25 11:15:29 +0100

    user service

--- src/main/java/com/taderok/taderok/Controller/PreLoginController.java
+++ src/main/java/com/taderok/taderok/Controller/PreLoginController.java
@@ -1,6 +1,9 @@
 package com.taderok.taderok.Controller;
 
 import com.taderok.taderok.Domain.Response;
+import com.taderok.taderok.Entity.Etudiant;
+import com.taderok.taderok.Entity.Parent;
+import com.taderok.taderok.Entity.Prof;
 import com.taderok.taderok.Entity.User;
 import com.taderok.taderok.LoginDocs.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +26,34 @@ public class PreLoginController {
         User dbUser = userService.save(user);
         if(dbUser!=null){
             return new ResponseEntity<Response>(new Response("User is saved"), HttpStatus.OK);
+        }
+        return null;
+    }
+
+    @PostMapping(value = "/registerEtudiant")
+    public ResponseEntity<Response> registrationEtudiant (@RequestBody Etudiant etudiant){
+        Etudiant dbUser = userService.registerEtudiant(etudiant);
+        if(dbUser!=null){
+            return new ResponseEntity<Response>(new Response("Etudiant is saved"), HttpStatus.OK);
+        }
+        return null;
+    }
+
+    @PostMapping(value = "/registerProf")
+    public ResponseEntity<Response> registrationProf (@RequestBody Prof prof){
+        Prof dbUser = userService.registerProf(prof);
+        if(dbUser!=null){
+            return new ResponseEntity<Response>(new Response("Prof is saved"), HttpStatus.OK);
+        }
+        return null;
+    }
+
+
+    @PostMapping(value = "/registerParent")
+    public ResponseEntity<Response> registrationParent (@RequestBody Parent parent){
+        Parent dbUser = userService.registerParent(parent);
+        if(dbUser!=null){
+            return new ResponseEntity<Response>(new Response("Parent is saved"), HttpStatus.OK);
         }
         return null;
     }
