f453e6b024234c73bffabeb73001e90fe73f21c3
ahmedBoukhtioua <ahmed.boukhtioua@esprit.tn>
2019-07-01 18:03:28 +0200

    upload changes

--- src/main/java/com/taderok/taderok/Service/Upload/StorageService.java
+++ src/main/java/com/taderok/taderok/Service/Upload/StorageService.java
@@ -19,7 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class StorageService {
 
 	Logger log = LoggerFactory.getLogger(this.getClass().getName());
-	private final Path rootLocation = Paths.get("C:/xampp/htdocs/tadderok");
+	private final Path rootLocation = Paths.get("C:/Users/hp/Documents/GitHub/taderok.com/taderok-front/light/src/assets/images/upload/uploadRessources/");
 
 	public void store(MultipartFile file) {
 		try {
