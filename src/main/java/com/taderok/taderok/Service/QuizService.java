f8a82a2e0c4c88bc2229c55fad6b433529d95654
chaimaB <chaima.braiek@esprit.tn>
2019-07-02 12:33:05 +0100

    fix error

--- src/main/java/com/taderok/taderok/Service/QuizService.java
+++ src/main/java/com/taderok/taderok/Service/QuizService.java
@@ -3,6 +3,7 @@ package com.taderok.taderok.Service;
 import com.taderok.taderok.Controller.AthenticationController;
 import com.taderok.taderok.Controller.HelloResource;
 import com.taderok.taderok.Entity.*;
+import com.taderok.taderok.Repository.ProfRepository;
 import com.taderok.taderok.Repository.ReponseRepository;
 import com.taderok.taderok.Repository.UserRepository;
 import com.taderok.taderok.Repository.QuizRepository;
@@ -20,6 +21,8 @@ public class QuizService {
     private ReponseRepository reponseRepository;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ProfRepository profRepository;
     @Autowired
     private HelloResource helloResource;
 
@@ -55,7 +58,9 @@ public class QuizService {
         /*User u = userRepository.findById(helloResource.getIdConnected()).orElse(null);
         quiz.setId_prof(u.getId()); */
         User u = userRepository.findById((long) AthenticationController.getConnectedUser().getUser().getId()).orElse(null);
-        Prof p = new Prof(u);
+        Prof p = profRepository.findById((long) AthenticationController.getConnectedUser().getUser().getId()).orElse(null);
+        System.out.println("prof : "+p);
+        System.out.println("user : "+u);
         quiz.setId_prof(p);
         quizRepository.save(quiz);
     }
