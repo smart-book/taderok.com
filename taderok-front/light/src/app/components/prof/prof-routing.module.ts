b121ef224532b2a2e4853a8eddc73fe96dd1cbd3
ahmedBoukhtioua <ahmed.boukhtioua@esprit.tn>
2019-07-04 14:27:12 +0200

    afficher/ajouter ressource final /appcomponent

--- taderok-front/light/src/app/components/prof/prof-routing.module.ts
+++ taderok-front/light/src/app/components/prof/prof-routing.module.ts
@@ -54,7 +54,7 @@ const routes: Routes = [
     component: AjouterRessourceComponent
   },
   {
-    path: 'AfficherRessource',
+    path: 'afficherRessource',
     component: AfficherRessourceComponent
   },
   {
