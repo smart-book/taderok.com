4d68888c64f89aaaec5df29fb91638a70b6cbacc
hannachioumeima007@gmail.com <arcades2008>
2019-07-04 03:47:44 -0700

    affichage avec angular material table

---
+++ taderok-front/light/src/app/components/etudiant/feedback/afficher-feedback/afficher-feedback.component.ts
@@ -0,0 +1,33 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatSort, MatTable } from '@angular/material';
+import { AfficherFeedbackDataSource } from './afficher-feedback-datasource';
+import {FeedbackService} from "../../../../services/etudiant/feedback.service";
+import {Feedback} from "../../../../models/feedback";
+
+@Component({
+  selector: 'app-afficher-feedback',
+  templateUrl: './afficher-feedback.component.html',
+  styleUrls: ['./afficher-feedback.component.css']
+})
+export class AfficherFeedbackComponent implements AfterViewInit, OnInit {
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
+  @ViewChild(MatTable, {static: false}) table: MatTable<Feedback>;
+  dataSource: AfficherFeedbackDataSource;
+
+  constructor(private feedbackService: FeedbackService) {
+  }
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['Nom','Prenom','E-mail','Type', 'Description','Matiere'];
+
+  ngOnInit() {
+    this.dataSource = new AfficherFeedbackDataSource(this.feedbackService);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
