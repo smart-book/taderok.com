c7adbefe714c5cb086411aeb71e4a87c8b5a5582
EttaherMaha <maha.ettaher@esprit.tn>
2019-07-08 11:36:14 +0100

    Maha

--- taderok-front/light/src/app/components/prof/prof.module.ts
+++ taderok-front/light/src/app/components/prof/prof.module.ts
@@ -9,7 +9,7 @@ import { AjouterQuizComponent } from './quiz/ajouter-quiz/ajouter-quiz.component
 import {ReactiveFormsModule} from "@angular/forms";
 import {AjouterComponentGroupe} from './groupes/ajouter/ajouter.component';
 import {AjouterComponentSeance} from './seance/ajouter/ajouter.component';
-import {MatFormFieldModule, MatAutocompleteModule, MatSelectModule} from "@angular/material";
+import {MatFormFieldModule, MatAutocompleteModule, MatSelectModule, MatTableModule, MatPaginatorModule, MatSortModule} from "@angular/material";
 import { ListerComponent } from './reclamation/lister/lister.component';
 import { AjouterRessourceComponent } from './ressource/ajouter-ressource/ajouter-ressource.component';
 import { AfficherRessourceComponent } from './ressource/afficher-ressource/afficher-ressource.component';
@@ -21,6 +21,7 @@ import {AfficherFeedbackComponent} from "./feedback/afficher-feedback/afficher-f
 import {SweetAlert2Module} from "@sweetalert2/ngx-sweetalert2";
 import {FullCalendarModule} from "@fullcalendar/angular";
 import { CalendarProfComponent } from './calendar-prof/calendar-prof.component';
+import { AfficherReclamationComponent } from './reclamation/afficher-reclamation/afficher-reclamation.component';
 
 
 
@@ -28,7 +29,7 @@ import { CalendarProfComponent } from './calendar-prof/calendar-prof.component';
 
 @NgModule({
 
-  declarations: [AfficherFeedbackComponent,AjouterComponent, AjouterComponentGroupe, AjouterComponentSeance, ListSeanceComponent, ListGroupeComponent, AjouterRessourceComponent, AfficherRessourceComponent,AjouterComponent, AjouterComponentGroupe,AjouterComponentSeance, ListerComponent ,ListQuizComponent,AjouterQuizComponent,CalendarProfComponent],
+  declarations: [AfficherFeedbackComponent,AjouterComponent, AjouterComponentGroupe, AjouterComponentSeance, ListSeanceComponent, ListGroupeComponent, AjouterRessourceComponent, AfficherRessourceComponent,AjouterComponent, AjouterComponentGroupe,AjouterComponentSeance, ListerComponent ,ListQuizComponent,AjouterQuizComponent,CalendarProfComponent, AfficherReclamationComponent],
 
 
 
@@ -47,7 +48,10 @@ import { CalendarProfComponent } from './calendar-prof/calendar-prof.component';
       confirmButtonClass: 'btn btn-primary',
       cancelButtonClass: 'btn'
     }),
-    FullCalendarModule
+    FullCalendarModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ]
 })
 export class ProfModule { }
