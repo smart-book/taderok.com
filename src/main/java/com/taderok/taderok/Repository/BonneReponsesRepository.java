ee1681b7da2dcfa3749755734a05d2bc8b40337f
Your Name <raef.lebdi@esprit.tn>
2019-07-11 15:38:55 +0200

    repo reponse

---
+++ src/main/java/com/taderok/taderok/Repository/BonneReponsesRepository.java
@@ -0,0 +1,10 @@
+package com.taderok.taderok.Repository;
+
+import com.taderok.taderok.Entity.BonneReponses;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BonneReponsesRepository extends CrudRepository<BonneReponses,Integer> {
+}
