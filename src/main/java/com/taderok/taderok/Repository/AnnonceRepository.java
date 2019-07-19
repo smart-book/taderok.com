625adfc0bf8a96c2575ab3c1d45acb0e0bde6303
unknown <skander.ellouze@esprit.tn>
2019-07-19 12:21:02 +0100

    Annonce back

---
+++ src/main/java/com/taderok/taderok/Repository/AnnonceRepository.java
@@ -0,0 +1,7 @@
+package com.taderok.taderok.Repository;
+
+import com.taderok.taderok.Entity.Annonce;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AnnonceRepository extends CrudRepository<Annonce,Long> {
+}
