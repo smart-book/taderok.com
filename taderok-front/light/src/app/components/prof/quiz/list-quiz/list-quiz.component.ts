9a69b1e79ac19089113b7adcb6cdccb9e1142e14
hannachioumeima007@gmail.com <arcades2008>
2019-07-22 03:15:43 -0700

    affichage List Quiz

--- taderok-front/light/src/app/components/prof/quiz/list-quiz/list-quiz.component.ts
+++ taderok-front/light/src/app/components/prof/quiz/list-quiz/list-quiz.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatSort, MatPaginator} from '@angular/material';
 import {Quiz} from "../../../../models/quiz";
-import {ListQuizService} from "../../../../services/prof/list-quiz.service";
 import {QuizService} from "../../../../services/prof/quiz.service";
 import {Question} from "../../../../models/Question";
 declare const $: any;
@@ -11,15 +11,59 @@ declare const $: any;
 })
 export class ListQuizComponent implements OnInit {
 
-  quizzes: Object[];
+  dataSource : MatTableDataSource<Quiz>;
+
+  @ViewChild (MatPaginator, {static: true}) paginator : MatPaginator;
+  @ViewChild (MatSort, {static: true}) sort: MatSort;
+
+  displayedColumns = ['nom', 'date'];
+
   quiz: Object;
   question: Question = new Question();
   etat = false;
   etat2 = true;
 
+  value: string;
+
   constructor(private quizService: QuizService) { }
 
   ngOnInit() {
+    setTimeout(()=>{
+      this.quizService.getAllQuiz().subscribe(data => {
+        this.dataSource = new MatTableDataSource(data);
+        console.log(this.dataSource.data);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sortingDataAccessor = (item, property) => {
+          switch(property) {
+            case 'nom': return item.nomQuiz;
+            case 'date': return item.date;
+            default: return item[property];
+          }
+        };
+        this.dataSource.sort = this.sort;
+        // @ts-ignore
+        this.dataSource.filterPredicate = (order: Order, filter: string) => {
+          const transformedFilter = filter.trim().toLowerCase();
+
+          const listAsFlatString = (obj): string => {
+            let returnVal = '';
+
+            Object.values(obj).forEach((val) => {
+              if (typeof val !== 'object') {
+                returnVal = returnVal + ' ' + val;
+              } else if (val !== null) {
+                returnVal = returnVal + ' ' + listAsFlatString(val);
+              }
+            });
+
+            return returnVal.trim().toLowerCase();
+          };
+
+          return listAsFlatString(order).includes(transformedFilter);
+        };
+
+      });
+    });
     this.quizService.getAllQuiz().subscribe(data => {this.quizzes=data; console.log(this.quizzes)});
   }
 
@@ -40,6 +84,15 @@ export class ListQuizComponent implements OnInit {
 
   findQuiz(id){
     this.quizService.findQuiz(id).subscribe(data=> {this.quiz=data; console.log(this.quiz)});
+  }
+
+  applyFilter(filterValue: string) {
+    const filters = filterValue.trim().toLowerCase();
+    this.dataSource.filter = filters;
+  }
+
+  OnSearchClear(){
+    this.value='';
   }
 
 }
