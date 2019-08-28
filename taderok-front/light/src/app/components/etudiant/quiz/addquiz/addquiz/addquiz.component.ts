c1432b245ae82828714517fd860e1697b172a98e
Your Name <raef.lebdi@esprit.tn>
2019-08-28 15:38:19 +0200

    quiz front update

--- taderok-front/light/src/app/components/etudiant/quiz/addquiz/addquiz/addquiz.component.ts
+++ taderok-front/light/src/app/components/etudiant/quiz/addquiz/addquiz/addquiz.component.ts
@@ -2,7 +2,7 @@ import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 
 import {Quiz} from "../../../../../models/quiz";
 import {ListQuizService} from "../../../../../services/etudiant/list-quiz.service";
-
+import Swal from "sweetalert2";
 @Component({
   selector: 'app-addquiz',
   templateUrl: './addquiz.component.html',
@@ -11,6 +11,10 @@ import {ListQuizService} from "../../../../../services/etudiant/list-quiz.servic
 export class AddquizComponent implements  OnInit{
 
 
+  value: string;
+  timeLeft: number = 60;
+  interval;
+  rowId;
 
   allquiz: Quiz[];
 
@@ -19,4 +23,29 @@ export class AddquizComponent implements  OnInit{
   ngOnInit() {
   }
 
+  afficherNote() {
+
+    Swal.fire({
+      title: 'Votre note est 13/20',
+      animation: false,
+      customClass: {
+        popup: 'animated tada'
+      }
+    })
+
+
+  }
+
+
+  startTimer() {
+    this.interval = setInterval(() => {
+      if(this.timeLeft > 0) {
+        this.timeLeft--;
+      } else {
+        this.timeLeft = 0;
+        clearInterval(this.interval);
+
+      }
+    },1000)
+  }
 }
